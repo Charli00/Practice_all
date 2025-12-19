771b4c3c3b791ec007a56f18f07e515b2a58735f
peniel charles p <98569512+Charli00@users.noreply.github.com>
2025-12-19 05:01:08 +0000

    class

---
+++ class_object.py
@@ -0,0 +1,21 @@
+# #create a class called laptop and create a following variables and functions.
+# variables ==> price,processor,ram
+# create object HP,DELL,Lenovo
+
+
+class laptop:
+    price=''
+    processor=''
+    RAM=''
+
+HP=laptop()
+HP.price=25000
+HP.processor="intel"
+HP.RAM="8GB"
+print(HP.price)
+print(HP.processor)
+print(HP.RAM)
+
+DELL=laptop()
+
+Lenovo=laptop()
